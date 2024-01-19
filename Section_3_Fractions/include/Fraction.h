a2920ca490dbaf72966fbf5dcc678efc7ccf904f
aanichenko01 <58330164+aanichenko01@users.noreply.github.com>
2024-01-19 16:01:48 +0000

    created a class

--- Section_3_Fractions/include/Fraction.h
+++ Section_3_Fractions/include/Fraction.h
@@ -1,2 +1,17 @@
 #pragma once
 #include <string>
+
+class Fraction {
+    public:
+    Fraction(int a, int b);
+    Fraction reciprocal();
+    Fraction multiply(int a);
+    double toDouble();
+    std::string toString();
+    
+    private:
+    int num;
+    int denom;
+
+
+};
--- Section_3_Fractions/src/Fraction.cpp
+++ Section_3_Fractions/src/Fraction.cpp
@@ -1,1 +1,29 @@
 #include "Fraction.h"
+
+Fraction::Fraction(int a, int b): num(a), denom(b)
+{
+    
+}
+
+Fraction Fraction::reciprocal()
+{
+    return Fraction(denom, num);
+}
+
+Fraction Fraction::multiply(int a)
+{
+    return Fraction(num*a, denom);
+}
+
+double Fraction::toDouble()
+{
+    return (double)num/denom;
+}
+
+std::string Fraction::toString(){
+    if (num == 0){
+        return "0";
+    } else {
+        return std::to_string(num) + "/" + std::to_string(denom);
+    }
+}
