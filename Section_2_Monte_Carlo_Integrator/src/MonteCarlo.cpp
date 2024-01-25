9fad0580ef45b559b3f2aa160941ebac550f3d88
aanichenko01 <58330164+aanichenko01@users.noreply.github.com>
2024-01-25 13:00:07 +0000

    updated to take user input

--- Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -7,7 +7,7 @@ bool InsideUnitSphere(double x, double y, double z)
     return ((x*x + y*y + z*z) <= 1);
 }
 
-double IntegrateMonteCarlo3D(int n_points, double min, double max)
+double IntegrateMonteCarlo3D(int n_points, double min, double max, std::function<bool(double, double, double)> TestPoint)
 {
     int count = 0;
     // N.B. std::pow with integer arguments can be less efficient than
@@ -27,7 +27,7 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
         double x = generate_uniform_real();
         double y = generate_uniform_real();
         double z = generate_uniform_real();
-        if(InsideUnitSphere(x, y, z)) count++;
+        if(TestPoint(x, y, z)) count++;
     }
 
     return static_cast<double>(count) / n_points * VolCube;
@@ -38,7 +38,12 @@ int main()
 {
     int N_points = 10000;
     
-    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
+    std::cout << "Enter number of points to sample (default 1000): ";
+    std::cin >> N_points;
+    if(N_points == 0) N_points == 1000;
+    
+    // pass in our function as a function pointer
+    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0, &InsideUnitSphere);
 
     std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
 }
